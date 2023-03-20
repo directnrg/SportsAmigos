448a9089990e387fff7651727e888aefbfd670c2
Fabian Soto <directnrg@gmail.com>
2023-03-19 23:24:25 -0400

    Init react app client

---
+++ react_client/sports-amigos/src/App.js
@@ -0,0 +1,15 @@
+import './styles/App.css';
+
+function SportsAmigosApp() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Init Sport Amigos react app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default SportsAmigosApp;
