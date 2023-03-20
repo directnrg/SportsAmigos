6296a4f7ea9248b92a52d1da9e3b8495390048c9
josekg <efrenhdez9812@gmail.com>
2023-03-20 19:39:26 -0400

    PlayerEntrance component

---
+++ react_client/sports-amigos/src/components/PlayerEntrance.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class PlayerEntrance extends Component {
+  render() {
+    return (
+      <div>PlayerEntrance</div>
+    )
+  }
+}
+
+export default PlayerEntrance
