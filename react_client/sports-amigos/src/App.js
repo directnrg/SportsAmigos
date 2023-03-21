cfea81e4df16253a929682ea249cb04c6e59b1c0
DiegoHdz7 <diego.hdz.glez@gmail.com>
2023-03-20 20:21:07 -0400

    Conditional render for navbar- depending on isLoggedIn

--- react_client/sports-amigos/src/App.js
+++ react_client/sports-amigos/src/App.js
@@ -1,5 +1,6 @@
 import './styles/App.css';
 import {Route,Routes, Link} from "react-router-dom";
+import {React,useEffect, useState} from 'react'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
@@ -8,42 +9,112 @@ import SignUp from './components/SignUp';
 import HowToPlay from './components/HowToPlay';
 import Button from 'react-bootstrap/Button';
 import Login from './components/Login';
+import Footer from './components/Footer';
+import LeagueList from './components/LeagueList';
+import MyLeagues from './components/MyLeagues';
 
 function SportsAmigosApp() {
-  return (
-    
-    <>
-    
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand  as={Link} to="/">Sports Amigos</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link}  to="/how-to-play">HowToPlay</Nav.Link>
-            <Button as={Link}  to="/sign-up">Sign up</Button>{' '}
-            <Button as={Link}  to="/log-in">Log in</Button>{' '}
-           
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    
-      <div>
-        <Routes>
-          <Route index element={<Home/>} />
-          <Route path="sign-up" element={<SignUp/>} />   
-          <Route path="/how-to-play" element={<HowToPlay/>} />  
-          <Route path="/log-in" element={<Login/>} /> 
-          
-          {/*Add the route to your component if necessary */}
-        </Routes>
-      </div>
-      </>
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginToggle = ()=>{
+    setIsLoggedIn(!isLoggedIn)
+
+  }
+
+  useEffect(() => {
+    document.title = 'Sports Pool';
+  }, []);
+
+  //Display if the user has logged in
+  if(isLoggedIn){
+    return (
     
-  );
+      <>
+      
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand  as={Link} to="/">Sports Amigos</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link}  to="/how-to-play">How To Play</Nav.Link>
+              <Nav.Link as={Link}  to="/league-list">League List</Nav.Link>
+              <Nav.Link as={Link}  to="/my-leagues">My Leagues</Nav.Link>
+              <Button as={Link}  to="/" onClick={loginToggle} >Log Out</Button>{' '}
+             
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+        <div>
+          <Routes>
+            <Route index element={<Home/>} />
+            <Route path="sign-up" element={<SignUp/>} />   
+            <Route path="/how-to-play" element={<HowToPlay/>} />  
+            <Route path="/log-in" element={<Login/>} /> 
+            <Route path="/league-list" element={<LeagueList/>} />  
+            <Route path="/my-leagues" element={<MyLeagues/>} /> 
+            
+            
+            
+            {/*Add the route to your component if necessary */}
+          </Routes>
+        </div>
+  
+        <Footer></Footer>
+  
+       
+        </>
+  
+      
+    );
+
+  }
+  else{
+    return (
+    
+      <>
+      
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand  as={Link} to="/">Sports Amigos</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link}  to="/how-to-play">How To Play</Nav.Link>
+              <Button as={Link}  to="/sign-up">Sign up</Button>{' '}
+              <Button onClick={loginToggle}  >Log in</Button>
+              
+             
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+        <div>
+          <Routes>
+            <Route index element={<Home/>} />
+            <Route path="sign-up" element={<SignUp/>} />   
+            <Route path="/how-to-play" element={<HowToPlay/>} />  
+            <Route path="/log-in" element={<Login/>} /> 
+            
+            {/*Add the route to your component if necessary */}
+          </Routes>
+        </div>
+  
+        <Footer></Footer>
+  
+       
+        </>
+  
+      
+    );
+  }
+
 }
 
 export default SportsAmigosApp;
