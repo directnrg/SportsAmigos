f2f770fe598666b7dbb7add67f31a033f432ef2d
josekg <efrenhdez9812@gmail.com>
2023-03-21 14:01:01 -0400

    Upcoming games component

--- react_client/sports-amigos/src/App.js
+++ react_client/sports-amigos/src/App.js
@@ -6,13 +6,22 @@ import Container from 'react-bootstrap/Container';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import HowToPlay from './components/HowToPlay';
+import PlayerEntrance from './components/PlayerEntrance';
 import Button from 'react-bootstrap/Button';
 import Login from './components/Login';
+import { Helmet } from 'react-helmet';
 
 function SportsAmigosApp() {
   return (
     
     <>
+
+<Helmet>
+      <script
+    type="module"
+    src="https://widgets.api-sports.io/2.0.3/widgets.js">
+</script>
+      </Helmet>
     
     <Navbar bg="light" expand="lg">
       <Container>
@@ -22,6 +31,7 @@ function SportsAmigosApp() {
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link}  to="/how-to-play">HowToPlay</Nav.Link>
+            <Nav.Link as={Link}  to="/player-entrance">Player Entrance</Nav.Link>
             <Button as={Link}  to="/sign-up">Sign up</Button>{' '}
             <Button as={Link}  to="/log-in">Log in</Button>{' '}
            
@@ -35,11 +45,13 @@ function SportsAmigosApp() {
           <Route index element={<Home/>} />
           <Route path="sign-up" element={<SignUp/>} />   
           <Route path="/how-to-play" element={<HowToPlay/>} />  
+          <Route path="/player-entrance" element={<PlayerEntrance/>} />
           <Route path="/log-in" element={<Login/>} /> 
           
           {/*Add the route to your component if necessary */}
         </Routes>
       </div>
+
       </>
 
     
--- react_client/sports-amigos/src/components/PlayerEntrance.js
+++ react_client/sports-amigos/src/components/PlayerEntrance.js
@@ -1,11 +1,47 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
 
-export class PlayerEntrance extends Component {
-  render() {
-    return (
-      <div>PlayerEntrance</div>
-    )
-  }
+export default function PlayerEntrance() {
+    const [data, setData] = useState([]);
+    const apiUrl = "https://v3.football.api-sports.io/fixtures?league=262&season=2022&round=Clausura - 13&timezone=America/Toronto";
+    useEffect(() => {
+      const fetchData = async () => {
+        axios.get(apiUrl, {
+          headers: {
+          "x-rapidapi-host": "v3.football.api-sports.io",
+		      "x-rapidapi-key": "e70700818b55ed8754c5f49cf75c67b5"
+          }
+          }).then(result => {
+            console.log('result.data:',result.data)
+              setData(result.data.response);
+          }).catch((error) => {
+            console.log('error in fetchData:', error)
+          });
+        };  
+      fetchData();
+    }, []);
+
+
+  return (
+    <>
+    <Table striped bordered hover variant="dark">
+    <thead>
+      <tr>
+        <th>Home</th>
+        <th>Away</th>
+      </tr>
+    </thead>
+    <tbody>
+    {Array.isArray(data) && data.map(fixture => (
+      <tr key={fixture.id}>
+        <td>{fixture.teams.home.name}</td>
+        <td>{fixture.teams.away.name}</td>
+      </tr>
+    ))}
+    </tbody>
+  </Table>
+  </>
+
+  )
 }
-
-export default PlayerEntrance
