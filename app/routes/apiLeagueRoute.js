c5c3cd1bd423ad276c8a6852caaa7037a055d56c
Fabian Soto <directnrg@gmail.com>
2023-04-09 17:38:10 -0400

    changed name file conventions for easy understanding and fixed affected sections of the code

--- app/routes/apiLeagueRoute.js
+++
@@ -1,8 +0,0 @@
-import { fecthMexicanLeagues } from "../controllers/apiLeagueController.js";
-import express from 'express';
-
-const apiLeagueRouter = express.Router();
-
-apiLeagueRouter.get("/mexican-leagues-current",fecthMexicanLeagues);
-
-export default apiLeagueRouter;
