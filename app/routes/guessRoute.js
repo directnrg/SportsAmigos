6d937dab80f52ee8fd0ec0cdd2290f7dc2601d29
Fabian Soto <directnrg@gmail.com>
2023-04-10 02:04:34 -0400

    implemented guess api

--- app/routes/guessRoute.js
+++ app/routes/guessRoute.js
@@ -1,20 +1,25 @@
 import { Router } from 'express';
-import { getAllGuesses, getGuessById, createGuess, updateGuess, deleteGuess } from '../controllers/guessController.js'
+import { getAllGuesses, createGuess, getGuessById,updateGuess, deleteGuess, addAllUserGuesses } from '../controllers/guessController.js'
+import { findGuessById } from '../middleware/guessMiddleware.js';
 
 const guessRouter = Router()
 
 // Create a new guess
 guessRouter.post('/guess', createGuess);
+
+// add all guesses from a user
+guessRouter.post('/user-guesses', addAllUserGuesses)
+
 // Get all guesses
 guessRouter.get('/guesses', getAllGuesses);
 
 // Get a single guess by id
-guessRouter.get('/guess/:guessId', getGuessById);
+guessRouter.get('/guess/:guessId', findGuessById, getGuessById);
 
 // Update a single guess by id
-guessRouter.patch('/guess/:guessId', updateGuess);
+guessRouter.patch('/guess/:guessId', findGuessById, updateGuess);
 
 // Delete a single guess by id
-guessRouter.delete('/guess/:guessId', deleteGuess);
+guessRouter.delete('/guess/:guessId', findGuessById, deleteGuess);
 
 export default guessRouter;
