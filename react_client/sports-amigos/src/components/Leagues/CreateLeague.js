823563c1ee83ab636de093f0a3d255bbd0c34a23
josekg <efrenhdez9812@gmail.com>
2023-04-10 15:42:30 -0400

    Home page, create league component

--- react_client/sports-amigos/src/components/Leagues/CreateLeague.js
+++ react_client/sports-amigos/src/components/Leagues/CreateLeague.js
@@ -8,6 +8,7 @@ import axios from 'axios';
 
 export default function CreateLeague() {
     const [users, setUsers] = useState([]);
+    const [weekGames, setWeekGames] = useState([]);
     const [league, setLeague] = useState({
       _id: '',
       name: '',
@@ -18,6 +19,7 @@ export default function CreateLeague() {
     });
   
     const usersUrl = 'http://localhost:3100/api/users/';
+    const gamesUrl = 'http://localhost:3100/api/games/';
   
     useEffect(() => {
       axios
@@ -29,6 +31,17 @@ export default function CreateLeague() {
         })
         .catch((err) => console.error(err));
     }, []);
+
+    useEffect(() => {
+      axios
+      .get(gamesUrl)
+      .then((res) => {
+        const gameIds = res.data.games.map((game) => game._id);
+        console.log('gameids', gameIds);
+        setWeekGames(gameIds);
+      })
+      .catch((err) => console.error(err));
+    }, []);
   
     const handleNameChange = (event) => {
       setLeague({ ...league, name: event.target.value });
@@ -39,9 +52,9 @@ export default function CreateLeague() {
     };
   
     const handleUserSelection = (event) => {
-      const selectedUserId = parseInt(event.target.value);
+      const selectedUserId = event.target.value;
       const updatedUsers = users.map((user) =>
-        user.id === selectedUserId ? { ...user, isSelected: event.target.checked } : user
+        user._id === selectedUserId ? { ...user, isSelected: event.target.checked } : user
       );
       setUsers(updatedUsers);
     };
@@ -53,14 +66,16 @@ export default function CreateLeague() {
   
       const data = {
         name: league.name,
-        users: selectedUsers.map((user) => user.id),
+        users: selectedUsers.map((user) => user._id),
         isPrivate: league.isPrivate,
+        games: weekGames
       };
   
       axios
-        .post('http://localhost:3100/api/leagues', data)
+        .post('http://localhost:3100/api/league', data)
         .then((response) => {
           console.log(response.data);
+          console.log(weekGames);
         })
         .catch((error) => {
           console.log(error);
@@ -88,11 +103,11 @@ export default function CreateLeague() {
             <Col>
               <h1>Users available:</h1>
               {users.map((user) => (
-                <Form.Group key={user.id} controlId={`user-${user.id}`}>
+                <Form.Group key={user._id} controlId={`user-${user._id}`}>
                   <Form.Check
                     type="checkbox"
                     label={user.fullName}
-                    value={user.id}
+                    value={user._id}
                     checked={user.isSelected}
                     onChange={handleUserSelection}
                   />
@@ -107,84 +122,3 @@ export default function CreateLeague() {
       </Container>
     );
   }
-
-/*export default function CreateLeague() {
-
-    const [users, setUsers] = useState([]);
-
-    const [league, setLeague] = useState({ _id: '', name: '', users: [], guesses: [], games: [], isPrivate: false});
-
-    const usersUrl = "http://localhost:3100/api/users/";
-
-    useEffect(() => {
-        axios
-          .get(usersUrl)
-          .then((res) => {
-            setUsers(res.data);
-            //debug
-            console.log(res.data);
-          })
-          .catch((err) => console.error(err));
-      }, []);
-
-      const handleSubmit = (event) => {
-        event.preventDefault();
-      
-        const selectedUsers = users.filter((user) => user.isSelected);
-      
-        const data = {
-          name: league.name,
-          users: selectedUsers.map((user) => user.id),
-          isPrivate: league.isPrivate,
-        };
-      
-        axios
-          .post('http://localhost:3100/api/leagues', data)
-          .then((response) => {
-            console.log(response.data);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      };
-
-  return (
-    <Container>
-        <Row>
-         <h1>Create a League</h1>
-        </Row>
-            <Form onSubmit={handleSubmit}>
-            <Row>
-                <Col>
-                <Form.Group className="mb-3" controlId="formLeagueName">
-                <Form.Label>League Name:</Form.Label>
-                <Form.Control type="text" placeholder="Enter league's name" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Label>Type of league:</Form.Label>
-                    <Form.Check type="checkbox" label="Private" />
-                    <Form.Check type="checkbox" label="Public" />
-                </Form.Group>
-                </Col>
-                <Col>
-                <h1>Users available:</h1>
-                {users.map(user => (
-                    <FormGroup key={user.id} controlId={`user-${user.id}`}>
-                    <FormCheck type="checkbox" label={user.fullName} />
-                    </FormGroup>
-                ))}
-                </Col>
-                </Row>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-
-            </Form>
-
-
-    </Container>
-
-  )
-}
-*/
