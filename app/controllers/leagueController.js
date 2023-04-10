32ee275b0e9837a4ab479ee65c8ecae7baa50133
Fabian Soto <directnrg@gmail.com>
2023-04-10 16:39:42 -0400

    implemented fixes for getting a league by user id and  added logic to populate league based on games of week from db

--- app/controllers/leagueController.js
+++ app/controllers/leagueController.js
@@ -1,4 +1,5 @@
 import League from '../models/league.js';
+import Guess from '../models/guess.js';
 
 export const createLeague = async (req, res) => {
   try {
@@ -9,6 +10,45 @@ export const createLeague = async (req, res) => {
     res.status(500).json({ message: 'Internal server error', error });
   }
 };
+
+export const getMyLeague = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const leagues = await League.find({ users: userId })
+      .populate(['games', 'guesses'])
+      .exec();
+
+    const guesses = await Guess.find({ user: userId }).populate('game').exec();
+
+    const games = [];
+    const isPrivate = [];
+    const leagueNames = [];
+    leagues.forEach((league) => {
+      league.games.forEach((game) => {
+        if (!games.some((g) => g._id.toString() === game._id.toString())) {
+          games.push(game);
+        }
+      });
+      isPrivate.push(league.isPrivate);
+      leagueNames.push(league.name);
+    });
+
+    const myLeagueView = {
+      user: userId,
+      guesses,
+      games,
+      isPrivate,
+      leagueNames,
+    };
+
+    res.status(200).json(myLeagueView);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 
 export const getLeagues = async (req, res) => {
   try {
